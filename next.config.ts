3667bfb3ac4ce81b420072243e4e5097a7a48d18
Saurabh Jindal <jindalsaurabh@gmail.com>
2024-11-19 19:02:39 +0100

    next-start

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /*
   experimental: {
     typedRoutes: true,
   },
+  */
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1> Hello World !! I am here !!! And you?</h1>;
+  return <h1> Hello World !! I am here !!! And you2?</h1>;
     }
